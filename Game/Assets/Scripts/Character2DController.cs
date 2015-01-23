9451c6b71e4ec6f58d09db868bbb5c6fbc3124c4
Sparox <lp-57@hotmail.fr>
2015-01-23 21:51:44 +0100

    Add player, action (jump), compile first scene, fixing collision between players

---
+++ Game/Assets/Scripts/Character2DController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySampleAssets._2D;
+
+
+[RequireComponent(typeof (PlatformerCharacter2D))]
+public class Character2DController : MonoBehaviour {
+
+	private bool jump;
+	public KeyCode actionKey;
+	public KeyCode leftKey;
+	public KeyCode rightKey;
+	private PlatformerCharacter2D character;
+	public Transform BlocPrefab;
+	private bool canCreateBlock = true;
+
+	private enum actionType
+	{
+		Jump
+	};
+
+	private actionType type;
+
+	private float direction = 0f;
+
+	private void Awake()
+	{
+		character = GetComponent<PlatformerCharacter2D> ();
+		type = actionType.Jump;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(leftKey))
+		{
+			direction = -1f;
+		} 
+		else if (Input.GetKey(rightKey))
+		{
+			direction = 1f;
+		}
+		else
+		{
+			direction = 0f;
+		}
+
+		if(Input.GetKeyDown(actionKey))
+		{
+			switch (type) {
+				case actionType.Jump:
+					jump = true;
+				break;
+				default:
+				break;
+			}
+		}
+	}
+
+	private void GenerateCube()
+	{
+		var posNewBloc = new Vector3 (this.transform.position.x, this.transform.localPosition.y - 2, this.transform.position.z);
+		//var newBloc = Instantiate (BlocPrefab, posNewBloc, Quaternion.identity);
+	}
+
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		float h = direction;
+		// Pass all parameters to the character control script.
+		character.Move(h, false, jump);
+
+		jump = false;
+		/*if(!canCreateBlock && character.IsGrounded())
+		{
+			canCreateBlock = true;
+		}*/
+	}
+}
--- Game/Assets/Scripts/PlatformerCharacter2D.cs
+++ Game/Assets/Scripts/PlatformerCharacter2D.cs
@@ -13,7 +13,7 @@ namespace UnitySampleAssets._2D
         [Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;
                                                      // Amount of maxSpeed applied to crouching movement. 1 = 100%
 
-        [SerializeField] private bool airControl = false; // Whether or not a player can steer while jumping;
+        [SerializeField] private bool airControl = true; // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask whatIsGround; // A mask determining what is ground to the character
 
         private Transform groundCheck; // A position marking where to check if the player is grounded.
